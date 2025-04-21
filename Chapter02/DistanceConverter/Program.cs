75235d72c4a5dcec2f4b6df870becfec10a39099
infosys <infosys@C4237>
2025-04-21 09:57:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.Metrics;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int start = int.Parse(args[1]);
+            int end = int.Parse(args[2]);
+
+            if (args[0] == "-tom") {
+                PrintFeelToMeterList(start, end);
+            } else {
+                PrintMeterToFeelList(start, end);
+            }
+        }
+
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
+        }
+        //フィートからメートルへと対応表を出力
+        static void PrintFeelToMeterList(int start, int end) {
+            for (int feet = start; feet <= end; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft ={meter: 0.0000}m");
+            }
+        }
+        //メートルからフィートへと対応表を出力
+        static void PrintMeterToFeelList(int start, int end) {
+            for (int meter = start; meter <= end; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}ft ={feet: 0.0000}m");
+            }
+        }
+
+
+    }
+}
+
+
