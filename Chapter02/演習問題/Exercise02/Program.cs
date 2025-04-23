a5cbdddae5984f8c2fe5ad1275ccce352d7a815d
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-04-23 09:33:26 +0900

    インチからメートル変換完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -6,17 +6,13 @@ namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
 
-            //int start = int.Parse(Console.ReadLine());
-            //int end = int.Parse(Console.ReadLine());
-
             Console.Write("はじめ:");
             int start = int.Parse(Console.ReadLine());
             Console.Write("おわり:");
             int end = int.Parse(Console.ReadLine());
             PrintInchToMeterList(start, end);
-            PrintMeterToInchlist(start, end);
+            //   PrintMeterToInchlist(start, end);
         }
-
 
         //インチからメートルへと対応表を出力。
         static void PrintInchToMeterList(int start, int end) {
@@ -25,11 +21,13 @@ namespace Exercise02 {
                 Console.WriteLine($"{inch}inch ={meter: 0.0000}m");
             }
         }
-        static void PrintMeterToInchlist(int start, int end) {
-            for (int meter = start; meter <= end; meter++) {
-                double inch = InchConverter.FromMeter(meter);
-                Console.WriteLine($"{meter}m ={inch: 0.0000}inch");
-            }
-        }
+
+        //メートルからインチへと対応表を出力。
+        //static void PrintMeterToInchlist(int start, int end) {
+        //    for (int meter = start; meter <= end; meter++) {
+        //        double inch = InchConverter.FromMeter(meter);
+        //        Console.WriteLine($"{meter}m ={inch: 0.0000}inch");
+        //    }
+        //}
     }
 }
