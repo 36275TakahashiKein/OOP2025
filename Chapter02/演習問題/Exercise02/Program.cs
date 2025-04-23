14fd9ed82db613288f713e95b764d2288caf8aa5
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-04-23 11:14:22 +0900

    機能追加②完成（修正）

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -11,7 +11,7 @@ namespace Exercise02 {
                 Console.Write("変換前の値（ヤード）：");
                 int yard = int.Parse(Console.ReadLine());
                 PrintYardToMeterList(yard);
-            } else if (start == 2){ 
+            } else if (start == 2) {
                 Console.Write("変換前の値（メートル）：");
                 int meter = int.Parse(Console.ReadLine());
                 PrintMeterToYardList(meter);
@@ -23,13 +23,14 @@ namespace Exercise02 {
         //ヤードからメートルへと対応表を出力。
         static void PrintYardToMeterList(int yard) {
             double meter = YardConverter.ToYard(yard);
-            Console.WriteLine("変換後（メートル）：" + meter);
+            Console.WriteLine($"変換後（メートル）： + {meter: 0.0000}");
         }
+
 
         //メートルからヤードへと対応表を出力。
         static void PrintMeterToYardList(int meter) {
             double yard = YardConverter.FromMeter(meter);
-            Console.WriteLine("変換後（ヤード）：" + yard);
+            Console.WriteLine($"変換後（ヤード）： + {yard: 0.0000}");
         }
     }
 }
