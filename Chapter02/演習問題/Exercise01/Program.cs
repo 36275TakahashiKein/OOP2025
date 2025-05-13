0ebb31ccc0fa3c8eca5caa0678607d2f68e15faa
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-05-13 10:12:24 +0900

    演習問題2.1.1

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise01 {
+    public  class Program {
+        static void Main(string[] args) {
+            //2.1.3
+
+
+
+
+
+
+
+
+        }
+
+        //2.1.4
+        private static void printSongs(Song[] songs) {
+
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1.
+    public class Song {
+        public string Title { get; set; } = string.Empty;
+        public string ArteisName { get; set; } = string.Empty;
+        public int Length { get; set; } 
+    }
+
+    //2.1.2
+
+
+}
