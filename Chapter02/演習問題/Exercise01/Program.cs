69fbf1306f1f372ff8d921f67a3ae76ce46bf5cd
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-05-13 11:13:30 +0900

    演習2.1.4

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -15,7 +15,10 @@
 
         //2.1.4
         private static void printSongs(Song[] songs) {
-            
+            for (var i = 0; i < songs.Length; i++) {
+                TimeSpan duration = TimeSpan.FromSeconds(songs[i].Length);
+                Console.WriteLine($"{songs[i].Title}{songs[i].ArtistName}:{duration.Minutes:D1}:{duration.Seconds:D2}");
+            }
         }
     }
 }
