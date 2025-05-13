c2dc829061b92a09344b5e179c794bd29ad57966
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-05-13 11:47:54 +0900

    2.4.3

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -15,15 +15,25 @@
 
         //2.1.4
         private static void printSongs(Song[] songs) {
+
+            //TimeSpan構造体を使った場合
             foreach (var song in songs) {
-                var minitue = song.Length / 60;
-                var seconds = song.Length % 60;
-                Console.WriteLine($"{song.Title},{song.ArtistName},{minitue}:{seconds:00}");
+                var timespan = TimeSpan.FromSeconds(song.Length);
+                Console.WriteLine($"{song.Title},{song.ArtistName},{timespan.Minutes}:{timespan.Seconds}");
             }
-            //for (var i = 0; i < songs.Length; i++) {
-            //    TimeSpan duration = TimeSpan.FromSeconds(songs[i].Length);
-            //    Console.WriteLine($"{songs[i].Title}{songs[i].ArtistName}:{duration.Minutes:D1}:{duration.Seconds:D2}");
-            //}
-        }
+
+            foreach (var song in songs) {
+                Console.WriteLine(@"{0},{1} {2:m\:ss}",
+                song.Title ,song.ArtistName ,TimeSpan.FromSeconds(song.Length));
+                //foreach (var song in songs) {
+                //    var minitue = song.Length / 60;
+                //    var seconds = song.Length % 60;
+                //    Console.WriteLine($"{song.Title},{song.ArtistName},{minitue}:{seconds:00}");
+                //}
+                //for (var i = 0; i < songs.Length; i++) {
+                //    TimeSpan duration = TimeSpan.FromSeconds(songs[i].Length);
+                //    Console.WriteLine($"{songs[i].Title}{songs[i].ArtistName}:{duration.Minutes:D1}:{duration.Seconds:D2}");
+                //}
+            }
     }
 }
