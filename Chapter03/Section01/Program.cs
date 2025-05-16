a6a8c24482e16a129e8e0d5f8ba2cd56fe741bac
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-05-16 14:56:47 +0900

    P71まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -3,26 +3,20 @@
 
         static void Main(string[] args) {
 
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-            Console.WriteLine(Count(numbers, n => n >= 5 && n <10));
-
+            var lowerList = cities.ConvertAll(s => s.ToUpper());
+            lowerList.ForEach(s => Console.WriteLine(s));
+            
         }
-
-
-        static int Count(int[] numbers, Func<int,bool> judge) {
-            var count = 0;
-
-            foreach (var n in numbers) {
-                //引数で受け取ったメソッドを呼び出す
-                if (judge(n) == true) {
-                    count++;
-                    Console.WriteLine(n);
-                }
-
-            }
-            return count;
-        }
-
     }
 }
