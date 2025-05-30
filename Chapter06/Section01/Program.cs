916d7c6f505c47e386ec8dbdda3b9d98d78ea6d6
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-05-30 15:34:55 +0900

    演習問題6.3.1と6.3.2完成

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var target = "C# Programming";
+            var isExists = target.Any(c => Char.IsLower(c));
+
+           
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            #region
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+            #endregion
+
+        }
+
+        private static void Exercise1(string text) {
+            string[] word = text.Split();
+            Console.Write("空白数：");
+            Console.WriteLine(word.Length - 1);
+            }
+   
+
+        private static void Exercise2(string text) {
+            Console.WriteLine(text.Replace("big", "small")); 
+        }
+
+        private static void Exercise3(string text) {
+            
+        }
+
+        private static void Exercise4(string text) {
+            string[] word = text.Split();
+            Console.WriteLine(word.Length);
+        }
+
+        private static void Exercise5(string text) {
+           
+            
+        }
+    }
+}
