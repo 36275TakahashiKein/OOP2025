d1597bc9497e29099aaacc61f110237a45a957cd
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-06-10 10:51:55 +0900

    演習問題5.2.2、5.2.3完成

---
+++ Chapter05/Section01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Immutable;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var obj = new MySample();
+
+            var newList = obj.MyList.Add(6).RemoveAt(0);
+            obj.MyList.ForEach(n => Console.Write($"{n}"));
+            Console.WriteLine();
+            newList.ForEach(n => Console.Write($"{n}"));
+            Console.WriteLine();
+        }
+    }
+
+    class MySample {
+        public ImmutableList<int> MyList { get; private set; }
+
+        public MySample() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            MyList = list.ToImmutableList();
+        }
+    }
+
+    public class Person {
+        public string GivenName { get; private set; }
+
+        public string FamilyName { get; private set; }
+
+        public Person(string familyName, string givenName) {
+            FamilyName = familyName;
+            GivenName = givenName;
+        }
+
+        //メソッド内でプロパティの値を変更できる
+        public void ChangeFamilyName(string name) {
+            FamilyName = name;
+        }
+    }
+
+}
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,57 @@
+﻿using Exercise01;
+using System.Runtime.CompilerServices;
+
+namespace Exercise02 {
+    public class Program {
+        static void Main(string[] args) {
+            // 5.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            FindFirst21C(ymCollection);
+
+            Console.WriteLine("5.2.4");
+            Exercise4(ymCollection);
+
+
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+        }
+
+        // 5.2.2
+        private static void Exercise2(YearMonth[] ymCollection) {
+            foreach (var item in ymCollection) {
+                Console.WriteLine(item);
+            }
+        }
+
+        // 5.2.3
+        //ここにメソッドを作成【メソッド名：FindFirst21C】
+        private static YearMonth? FindFirst21C(YearMonth[] ymCollection) {
+            foreach (var item in ymCollection) {
+                if (item.Is21Century) {
+                   
+                } 
+            }
+            return null;
+        }
+
+        // 5.2.4
+        private static void Exercise4(YearMonth[] ymCollection) {
+
+        }
+
+        // 5.2.5
+        private static void Exercise5(YearMonth[] ymCollection) {
+
+        }
+    }
+}
