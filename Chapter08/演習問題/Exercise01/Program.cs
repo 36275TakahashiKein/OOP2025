e8b4334846d0aca4d50d4fe61caeca4af749795f
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-06-11 11:04:01 +0900

    演習問題8.1.1完成

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            //ディクショナリインスタンスの作成
+            var dict = new Dictionary<char, int>();
+
+            //textを大文字にしつつ、一文字ずつ取り出す、dictにAddする。
+            foreach (var ch in text.ToUpper()) {
+                if('A' <= ch && ch <= 'Z') {
+                    //if、Keyが存在していたら。
+                    if (dict.ContainsKey(ch)) {
+                        var count = dict[ch];
+                        dict.Remove(ch);
+                        dict.Add(ch,count + 1);
+                    } else {
+                        dict.Add(ch,1);
+                    }
+                }
+            }
+            
+            //出力処理
+            foreach (var item in dict.OrderBy(s=> s.Key)) {
+                Console.WriteLine($"'{item.Key}': {item.Value}");
+            }
+        }
+
+        private static void Exercise2(string text) {
+            
+        }
+    }
+}
