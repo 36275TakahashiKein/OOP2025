0f105945f40b4f9f3b32dbf108e1bd8551a4a9be
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-06-11 11:13:36 +0900

    演習問題8.1.2完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -5,7 +5,7 @@ namespace Exercise01 {
             var text = "Cozy lummox gives smart squid who asks for job pen";
 
             Exercise1(text);
-            Console.WriteLine();
+            Console.WriteLine("********************");
 
             Exercise2(text);
 
@@ -17,26 +17,46 @@ namespace Exercise01 {
 
             //textを大文字にしつつ、一文字ずつ取り出す、dictにAddする。
             foreach (var ch in text.ToUpper()) {
-                if('A' <= ch && ch <= 'Z') {
+                if ('A' <= ch && ch <= 'Z') {
                     //if、Keyが存在していたら。
                     if (dict.ContainsKey(ch)) {
                         var count = dict[ch];
                         dict.Remove(ch);
-                        dict.Add(ch,count + 1);
+                        dict.Add(ch, count + 1);
                     } else {
-                        dict.Add(ch,1);
+                        dict.Add(ch, 1);
                     }
                 }
             }
-            
+
             //出力処理
-            foreach (var item in dict.OrderBy(s=> s.Key)) {
+            foreach (var item in dict.OrderBy(s => s.Key)) {
                 Console.WriteLine($"'{item.Key}': {item.Value}");
             }
         }
 
+
         private static void Exercise2(string text) {
-            
+            var dict = new SortedDictionary<char, int>();
+
+            //textを大文字にしつつ、一文字ずつ取り出す、dictにAddする。
+            foreach (var ch in text.ToUpper()) {
+                if ('A' <= ch && ch <= 'Z') {
+                    //if、Keyが存在していたら。
+                    if (dict.ContainsKey(ch)) {
+                        var count = dict[ch];
+                        dict.Remove(ch);
+                        dict.Add(ch, count + 1);
+                    } else {
+                        dict.Add(ch, 1);
+                    }
+                }
+            }
+
+            //出力処理
+            foreach (var item in dict) {
+                Console.WriteLine($"'{item.Key}': {item.Value}");
+            }
         }
     }
 }
