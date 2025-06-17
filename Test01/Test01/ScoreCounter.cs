7816db1ff77bcff5025da796d2e32ac6a81fd8bc
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-06-17 11:29:36 +0900

    設問1_Studentクラス完成

---
+++ Test01/Test01/ScoreCounter.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Test01 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
+        }
+
+
+        //メソッドの概要： 
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            //得点データを入れるリストオブジェクトを生成
+            var students = new List<Student>();
+            //ファイルを一気に読み込み
+            var lines = File.ReadAllLines(filePath);
+            //読み込んだ行数分繰り返し
+            foreach (var line in lines) {
+                string[] items = line.Split(',');
+                //Studentオブジェクトを生成
+                var sale = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(sale);
+            }
+            return students;
+
+
+
+
+        }
+
+        //メソッドの概要： 
+        public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (Student student in _score) {
+                if (dict.ContainsKey(student.Subject)) {
+                    dict[student.Subject] += student.Score;
+                } else {
+                    dict[student.Subject] = student.Score;
+                }
+
+            }
+            return dict;
+
+
+
+
+        }
+    }
+}
---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        //店舗名
+        public string Name { get; set; } = String.Empty;
+        //商品カテゴリー
+        public string Subject { get; set; } = String.Empty;
+        //売上高
+        public int Score { get; set; }
+
+
+
+    }
+}
