a711ae7f2f2a6260d3e53b936cfe4716eb872203
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-09-09 15:04:41 +0900

    ComboBox完成

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,20 +14,40 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ColorChecker
-{
+namespace ColorChecker {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+            DataContext = GetColorList();
+        }
+
+        /// <summary>
+        /// MainWindow.xaml の相互作用ロジック
+        /// </summary>
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
             colorArea.Background = new SolidColorBrush(Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value));
+        }
+
+       
+        //コンボボックスから色を選択
+        private void colorSelectComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var comboSelectMyColor = (MyColor)((ComboBox)sender).SelectedItem;
+            setSliderValue(comboSelectMyColor.Color);
+        }
+
+        //各スライダーの値を設定する
+        private void setSliderValue(Color color) {
+            rSlider.Value = color.R;
+            gSlider.Value = color.G;
+            bSlider.Value = color.B;
         }
     }
 }
--- WPF/ColorChecker/myColor.cs
+++ WPF/ColorChecker/myColor.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
 
 namespace ColorChecker {
-    public struct myColor {
+
+    public struct MyColor {
         public Color Color { get; set; }
         public string Name { get; set; }
         public override string ToString() {
