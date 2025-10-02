745fc932265ab36a765ac1e99d96c9f3a8f68131
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-10-02 15:47:36 +0900

    IsEnabled属性

--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -9,15 +9,28 @@ namespace HelloWorld {
     class ViewModel : BindableBase {
 
         public ViewModel() {
-            ChangeMessageCommand = new DelegateCommand(
-                () => GreetingMessage = "Bye-bye world");
+            ChangeMessageCommand = new DelegateCommand<string>(
+                (par) => GreetingMessage = par);
         }
 
         private string _greetingMessage = "HelloWorld";
         public string GreetingMessage {
             get => _greetingMessage;
-            set => SetProperty(ref _greetingMessage, value);
+            set {
+                if (SetProperty(ref _greetingMessage, value)) {
+                    CanChangeMessage = false;
+                }
+            }
         }
-        public DelegateCommand ChangeMessageCommand { get; }
+
+        private bool _canChangeMessage = true;
+        public bool CanChangeMessage {
+            get => _canChangeMessage;
+            private set => SetProperty(ref _canChangeMessage, value);
+        }
+
+        public string NewMessage1 { get; } = "Bye-bye world";
+        public string NewMessage2 { get; } = "Long time no see, world";
+        public DelegateCommand<string> ChangeMessageCommand { get; }
     }
 }
