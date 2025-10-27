6caf832d22aaf791f488dbb275b5aa6ab4bfca00
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-10-27 15:58:32 +0900

    cHAPTER13

---
+++ Chapter13/Section01/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var price = Library.Books
+                .Where(b => b.CategoryId == 1)
+                .Max(b => b.Price);
+            Console.WriteLine(price);
+
+            Console.WriteLine();
+
+            var book = Library.Books
+                .Where(b => b.PublishedYear >= 2021)
+                .MinBy(b => b.Price);
+            Console.WriteLine(book);
+
+            Console.WriteLine();
+
+            var average = Library.Books.Average(x => x.Price);
+            var aboves = Library.Books
+                .Where(b => b.Price > average);
+            foreach (var book1 in aboves) {
+                Console.WriteLine(book1);
+
+            }
+
+            var groups = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .OrderByDescending(g => g.Key);
+            foreach (var group in groups) {
+                Console.WriteLine($"{group.Key}年");
+                foreach (var book2 in group) {
+                    Console.WriteLine($"  {book}");
+
+                }
+            }
+
+            Console.WriteLine("---------");
+
+            var selected = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .Select(group => group.MaxBy(b => b.Price))
+                .OrderBy(b => b!.PublishedYear);
+
+            foreach (var book4 in selected) {
+                Console.WriteLine($"{book!.PublishedYear}年{book!.Title} ({book!.Price})");
+
+            }
+        }
+    }
+}
---
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var book2 = Library.Books
+                .MaxBy(b => b.Price);
+            Console.WriteLine(book2);
+        }
+
+        private static void Exercise1_3() {
+            var book3 = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .OrderByDescending(b => b.Key);
+            foreach (var book3s in book3) {
+                Console.WriteLine($"{book3s.Key}");
+                
+
+            }
+            
+        }
+
+        private static void Exercise1_4() {
+            
+        }
+
+        private static void Exercise1_5() {
+            
+        }
+
+        private static void Exercise1_6() {
+            
+        }
+
+        private static void Exercise1_7() {
+            
+        }
+
+        private static void Exercise1_8() {
+            
+        }
+    }
+}
