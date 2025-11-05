e00b07f8e31f79e18c7616b90d064139d8e6a024
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-11-05 11:16:47 +0900

    Code14_13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient hc = new HttpClient();
+            await GetHtmlExample(hc);
+        }
+
+        static async Task GetHtmlExample(HttpClient httpClient) {
+            var url = "https://www.yahoo.co.jp/";
+            var text = await httpClient.GetStreamAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
