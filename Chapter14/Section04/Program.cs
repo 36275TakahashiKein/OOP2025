552d4277701d4904077865bbe460ae78b90ed21c
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-11-05 11:39:44 +0900

    aaa

--- Chapter14/Section04/Program.cs
+++ Chapter14/Section04/Program.cs
@@ -1,8 +1,17 @@
-﻿namespace Section04 {
+﻿using System.Text.Json.Nodes;
+using System.Text.Json;
+
+namespace Section04 {
     internal class Program {
         static async Task Main(string[] args) {
             HttpClient hc = new HttpClient();
-            await GetHtmlExample(hc);
+
+            hc.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "MyWikipediaClient/1.0 (https://example.com/; contact@example.com)"
+                );
+
+            var text = await GetFromWikipediaAsync(hc,"伊勢崎市");
+            Console.WriteLine(text); 
         }
 
         static async Task GetHtmlExample(HttpClient httpClient) {
@@ -10,5 +19,41 @@
             var text = await httpClient.GetStreamAsync(url);
             Console.WriteLine(text);
         }
+
+        static async Task<string> GetFromWikipediaAsync(HttpClient httpClient, string keyword) {
+            // UriBuilderとFormUrlEncodedContentを使い、パラメーター付きのURLを組み立てる
+            var builder = new UriBuilder("https://ja.wikipedia.org/w/api.php");
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>() {
+                ["action"] = "query",
+                ["format"] = "json",
+                ["prop"] = "extracts",
+                ["redirects"] = "1",
+                ["explaintext"] = "1",
+                ["titles"] = keyword,
+            });
+            // JsonStringを取得する
+            builder.Query = await content.ReadAsStringAsync();
+            var jsonString = await httpClient.GetStringAsync(builder.Uri);
+
+            // JsonStringをパースして、コンテンツ文字列を取得する
+            return GetContentString(jsonString);
+        }
+
+        // JsonStringをパースして、コンテンツ文字列を取得する
+        static string GetContentString(string jsonString) {
+            var jsonNode = JsonNode.Parse(jsonString)!;
+            var node = jsonNode["query"]!["pages"]!;
+            var pagesElement = JsonSerializer.Deserialize<JsonElement>(node);
+            var name = GetChildPropertyName(pagesElement);
+            JsonElement contentElement = pagesElement.GetProperty(name);
+            JsonElement extractElement = contentElement.GetProperty("extract");
+            return extractElement.GetString() ?? "";
+        }
+
+        // element直下のキー名を取得する
+        static string GetChildPropertyName(JsonElement element) {
+            return element.EnumerateObject().First().Name;
+        }
+
     }
 }
