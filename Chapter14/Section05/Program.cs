5bd6e29391b747ea0f2fd40799a596ee721f8080
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-11-06 15:11:32 +0900

    aa

--- Chapter14/Section05/Program.cs
+++ Chapter14/Section05/Program.cs
@@ -1,7 +1,12 @@
 ﻿namespace Section05 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(1000);
+                Console.WriteLine($"処理{i}終了");
+            }
         }
     }
 }
