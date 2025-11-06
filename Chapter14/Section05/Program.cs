4c7733797222f7221d92d483b4bf63ae20a001a6
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-11-06 15:17:18 +0900

    並列処理ありとなしのサンプル

--- Chapter14/Section05/Program.cs
+++ Chapter14/Section05/Program.cs
@@ -2,6 +2,15 @@
     internal class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("並列処理あり");
+            Parallel.For(0,5,i => {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(1000);
+                Console.WriteLine($"処理{i}終了");
+            
+            });
+
+            Console.WriteLine("並列処理なし");
             for (int i = 0; i < 5; i++) {
                 Console.WriteLine($"処理{i}開始");
                 Thread.Sleep(1000);
