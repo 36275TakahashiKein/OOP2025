48eac094c17641ce0256022cf280b093c10e1d28
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-11-12 11:55:28 +0900

    ②

--- Chapter15/TempleteMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TempleteMethod/LineCounter/LineCounterProcessor.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 using TextFileProcessor;
 
 namespace LineCounter {
-    internal class LineCounterProcessor :TextProcessor {
+    internal class LineCounterProcessor : TextProcessor {
         private int _count = 0;
+        private string lolo;
 
-        protected override void Initialize(string fname) => _count = 0;
-
-        protected override void Execute(string line) {
-           if(line.Contains("public")) _count++; 
+        protected override void Initialize(string fname) {
+            _count = 0;
+            Console.WriteLine("任意の文字");
+            lolo = Console.ReadLine();
         }
 
-        protected override void Terminate() => Console.WriteLine("{0} 行",_count);
+        protected override void Execute(string line) {
+
+            if (line.Contains(lolo)) _count++;
+        }
+
+        protected override void Terminate() => Console.WriteLine("{0} 行", _count);
 
     }
 }
