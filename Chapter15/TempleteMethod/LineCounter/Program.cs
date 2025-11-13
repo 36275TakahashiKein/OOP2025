6b37e90e29ffd8af2cbd7da4881bf8a4f3fc6cf1
36275TakahashiKein <oibc36275@ojs.ac.jp>
2025-11-13 13:37:08 +0900

    パス指定の単語カウント

--- Chapter15/TempleteMethod/LineCounter/Program.cs
+++ Chapter15/TempleteMethod/LineCounter/Program.cs
@@ -3,7 +3,7 @@
 namespace LineCounter {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("ファイルのパスを指定してください");
+            Console.WriteLine("ファイルパスを指定してください");
             string path = Console.ReadLine();
             TextProcessor.Run<LineCounterProcessor>(fileName: path);
         }
